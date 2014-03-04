b9844579feaa8a3cf97b17ddf23173f0df435ac2
michelangelo <dragonbe+github@gmail.com>
2014-03-04 09:12:07 +0100

    Optimizing template for rendering

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -10,16 +10,24 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         } else {
             $view = new Zend_View();
         }
-        $view->headMeta()->setHttpEquiv(
-            'Content-Type', 'text/html; Charset=UTF-8'
-        );
+        $view->docType('HTML5');
+        $view->headMeta()
+            ->appendHttpEquiv('Content-Type', 'text/html; Charset=UTF-8')
+            ->appendHttpEquiv('X-UA-Compatible', 'IE=Edge')
+            ->appendName('viewport', 'width=device-width, initial-scale=1')
+            ->appendName('Description', 'This is an in2it training demo application')
+            ->appendName('Keywords', 'in2it, training, demo, zend framework, zf, zf1');
         $view->headTitle('Demo Application');
         $view->headTitle()->setSeparator(' | ');
         $view->headLink()
+            ->headLink(array (
+                'rel' => 'icon',
+                'href' => $view->baseUrl('/media/favicon.ico')
+            ))
             ->appendStylesheet($view->baseUrl('/css/bootstrap.min.css'))
             ->appendStylesheet($view->baseUrl('/css/bootstrap-theme.min.css'))
             ->appendStylesheet($view->baseUrl('/css/style.css'));
-        $view->headScript()
+        $view->inlineScript()
             ->appendFile($view->baseUrl('/js/jquery-1.11.0.min.js'))
             ->appendFile($view->baseUrl('/js/bootstrap.min.js'));
 
